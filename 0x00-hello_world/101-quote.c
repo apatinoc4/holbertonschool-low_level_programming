00ee0b01c66191d78be68b832d354930592c61a3
apatinoc4 <apatinoc4@gmail.com>
2019-06-19 21:38:41 +0000

    Extra 2 Corrected

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,9 +3,10 @@
 /**
  * main - Entry point
  *
- * Return: Always 1 (Success)
+ * Return: Always 1 (Error)
  */
 int main(void)
-{write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-1", 59);
+{
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-1", 59);
 return (1);
 }
