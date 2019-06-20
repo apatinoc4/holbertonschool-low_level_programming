45d7777b98f5ec0ef969b6385c5c178960be7828
apatinoc4 <apatinoc4@gmail.com>
2019-06-20 19:42:44 +0000

    Corrected 1 4th time

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -19,17 +19,12 @@ int mod;
 	mod = n % 10;
 	printf("Last digit of %d is %d ", n, mod);
 if (mod > 5)
-{
 	printf("and is greater than 5\n");
-}
-else if (mod != 0)
-{
-	printf("and is less than 6 and not 0\n");
-}
-else
-{
-	printf(" and is 0\n");
-}
 
-	return (0);
+else if (mod != 0)
+	printf("and is less than 6 and not 0\n");
+
+else
+	printf(" and is 0\n");
+return (0);
 }
