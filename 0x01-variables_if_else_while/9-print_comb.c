38ff3a5929d3f4b95d2011f584207871754885b4
apatinoc4 <apatinoc4@gmail.com>
2019-06-20 22:31:57 +0000

    Answered 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+*main - programs things
+*Result: always returns 0
+*Return: 0
+*
+ */
+
+
+int main(void)
+{
+int number;
+for (number = 0; number < 10; ++number)
+{
+putchar(number + '0');
+if (number != 9)
+{
+putchar(',');
+putchar(' ');
+}
+}
+putchar('\n');
+return (0);
+}
