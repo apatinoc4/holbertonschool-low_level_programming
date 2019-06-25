d4e79c06fdcb3fae812006b82656bf9fdfbfb763
apatinoc4 <apatinoc4@gmail.com>
2019-06-25 15:54:20 +0000

    answered 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+/**
+*print_alphabet - programs things
+*Result: always returns 0
+*Return: 0
+*
+ */
+
+void print_alphabet(void)
+{
+char g;
+for (g = 'a'; g <= 'z'; ++g)
+_putchar(g);
+_putchar('\n');
+}
