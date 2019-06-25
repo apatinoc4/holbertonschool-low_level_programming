1ae246ffed2c39fb2d8d96c3b391f4b2890c524b
apatinoc4 <apatinoc4@gmail.com>
2019-06-25 19:40:33 +0000

    Answered 6 , 7

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+/**
+*int _abs - programs things
+*@n : character defined
+*Result: always returns 0
+*Return: 0
+*
+ */
+
+int _abs(int n)
+{
+if (n > 0 )
+{
+return (n);
+}
+else
+{
+return (n * -1);
+}
+}
