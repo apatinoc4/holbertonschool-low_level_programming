88599ee6aa6a799252a2d2aa162b94bd4365daf7
apatinoc4 <apatinoc4@gmail.com>
2019-06-25 15:20:47 +0000

    Answered 0

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+*main - programs things
+*Result: always returns 0
+*Return: 0
+*
+ */
+
+int main(void)
+{
+char c[] = "Holberton";
+int i;
+for (i = 0; i < 9; ++i)
+{
+_putchar(c[i]);
+}
+_putchar('\n');
+return (0);
+}
