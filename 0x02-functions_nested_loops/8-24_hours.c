7b1f6c919210d51a109819fa15858aa48970e3c4
apatinoc4 <apatinoc4@gmail.com>
2019-06-26 02:02:28 +0000

    corrected 8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -10,28 +10,19 @@
 void jack_bauer(void)
 {
 int ho;
-int ht;
-int mo;
 int mt;
 
+
+for (ho = 0; ho <= 23; ++ho)
 {
-for (ho = 0; ho < 3; ho++)
+for (mt = 0; mt <= 59; mt++)
 {
-for (ht = 0; ht < 4; ht++)
-{
-for (mo = 0; mo < 6; mo++)
-{
-for (mt = 0; mt < 10; mt++)
-{
-_putchar(ho + '0');
-_putchar(ht + '0');
+_putchar(ho / 10 + '0');
+_putchar(ho % 10 + '0');
 _putchar(':');
-_putchar(mo + '0');
-_putchar(mt + '0');
+_putchar(mt / 10 + '0');
+_putchar(mt % 10 + '0');
 _putchar('\n');
-}
-}
-}
 }
 }
 }
