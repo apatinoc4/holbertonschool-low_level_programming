972bcf209f5d1f1723a721797ac8cfbb80dde71d
apatinoc4 <apatinoc4@gmail.com>
2019-06-27 19:03:29 +0000

    Answered 2-3

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+/**
+*mul - programs things
+*Result: always returns 0
+*@a : character determined by function
+*@b : character determined by function
+*Return: value depending on function
+*
+ */
+int mul(int a, int b)
+{
+return ((a * b));
+}
