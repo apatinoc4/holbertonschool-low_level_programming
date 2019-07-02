2e2fa72695a6555af29691defe093d6af1b90391
apatinoc4 <apatinoc4@gmail.com>
2019-07-02 16:09:43 +0000

    Answered 2-3

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,15 +1,16 @@
 #include "holberton.h"
 
 /**
-*_strlen - programs things
-*Result: always returns 0
-*@s : variable to define array length
-*Return: 0 sucess
-*
- */
+  *_strlen - programs things
+  *Result: always returns 0
+  *@s : variable to define array length
+  *Return: 0 sucess
+  *
+   */
 int _strlen(char *s)
 {
-char *str;
-str = "Holberton!";
-*s = _strlen (str);
+int b = 0;
+while (s[b] != '\0')
+b++;
+return (b);
 }
