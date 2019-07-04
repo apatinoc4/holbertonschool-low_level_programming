d7ab302481bc3f0111c1240ddf1a035a95d16a11
apatinoc4 <apatinoc4@gmail.com>
2019-07-04 22:28:00 +0000

    Answered 4

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+
+/**
+  *reverse_array - programs things
+  *Result: always returns 0
+  *@a : variable to define array length
+  *@n : variable to define array length
+  *Return: 0 sucess
+  *
+  */
+void reverse_array(int *a, int n)
+{
+int b;
+int c;
+int d;
+char e[1000];
+for (c = 0; c < n; c++)
+{
+e[c] = a[c];
+}
+for (b = 0; b < n; b++)
+{
+d = n - 1 - b;
+a[b] = e [d];
+d--;
+}
+}
