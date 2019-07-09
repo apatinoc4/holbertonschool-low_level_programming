5dc7de4d190abad290532fa27c11f70dde839202
apatinoc4 <apatinoc4@gmail.com>
2019-07-09 03:54:11 +0000

    Answered 8

---
+++ 0x07-pointers_arrays_strings/9-set_string.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+  *set_string - programs things
+  *Result: always returns 0
+  *@s : variable
+  *@to : variable
+  *Return: 0 sucess
+  *
+  */
+
+void set_string(char **s, char *to)
+{
+*s = to;
+}
