d8ba162b5123bcc8378012ec3054a68b9e2c0a25
apatinoc4 <apatinoc4@gmail.com>
2019-07-16 19:46:55 +0000

    Answered 1

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,35 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+  *_strdup - programs things
+  *Result: always returns 0
+  *@str : variable
+  *Return: 0 sucess
+  */
+
+
+char *_strdup(char *str)
+{
+	unsigned int a, b;
+	char *c;
+
+	if (str == NULL)
+	{
+		return (NULL);
+}
+	for (a = 0; str[a] != '\0'; a++)
+	{
+		a++;
+}
+	c = malloc(a * sizeof(char));
+		if (c == NULL)
+		{
+		return (NULL);
+}
+	for (b = 0; b < a; b++)
+	{
+		c[b] = str[b];
+}
+	return (c);
+}
