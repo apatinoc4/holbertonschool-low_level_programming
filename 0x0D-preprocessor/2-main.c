f07ea5c3119dd84bd2b4ae703b7d8cdc7e28e74e
apatinoc4 <apatinoc4@gmail.com>
2019-07-22 21:23:30 +0000

    Answered Questions 0-4

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * *main - programs things
+ * *Result: always returns 0
+ * *Return: 0 sucess
+   */
+
+int main(void)
+{
+		printf("%s\n", __FILE__);
+			return (0);
+}
