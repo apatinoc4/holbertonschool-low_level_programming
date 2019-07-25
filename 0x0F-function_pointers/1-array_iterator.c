20c3e832b38c672b8f2e0fb3c7c3addb74781cd7
apatinoc4 <apatinoc4@gmail.com>
2019-07-25 00:55:45 +0000

    Answered 0-1

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,19 @@
+#include "function_pointers.h"
+
+/**
+ * *array_iterator - programs things
+ * *Result: always returns 0
+ * *@array : variable
+ * *@size : variable
+ * *@action : variable
+ * *Return: 0 sucess
+   */
+void array_iterator(int *array, size_t size, void (*action)(int));
+{
+void (*ptofunct)(char *);
+if (name == NULL || f == NULL)
+return;
+ptofunct = f;
+ptofunct(name);
+}
+
