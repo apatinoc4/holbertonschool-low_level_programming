b76e2e8e95c3e80d8d3eb02caef4d63a5d514337
apatinoc4 <apatinoc4@gmail.com>
2019-07-25 01:05:03 +0000

    Fixed mixup between 0 alternative and 1

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,19 +1,22 @@
 #include "function_pointers.h"
 
 /**
- * *array_iterator - programs things
- * *Result: always returns 0
- * *@array : variable
- * *@size : variable
- * *@action : variable
- * *Return: 0 sucess
-   */
-void array_iterator(int *array, size_t size, void (*action)(int));
+  *array_iterator - programs things
+  *Result: always returns 0
+  *@array : variable
+  *@size : variable
+  *@action : variable
+  *Return: 0 sucess
+  */
+void array_iterator(int *array, size_t size, void (*action)(int))
 {
-void (*ptofunct)(char *);
-if (name == NULL || f == NULL)
-return;
-ptofunct = f;
-ptofunct(name);
-}
+size_t a;
 
+if (array && action)
+{
+for (a = 0; a < size; a++)
+{
+action(array[a]);
+}
+}
+}
