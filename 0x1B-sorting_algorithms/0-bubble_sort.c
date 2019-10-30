ba5721e2e35d5cd4f055c63a9c2ca2ae30e4f173
apatinoc4 <apatinoc4@gmail.com>
2019-10-30 16:50:19 -0500

    fused swap.c and 0 file for checker checking

--- 0x1B-sorting_algorithms/0-bubble_sort.c
+++ 0x1B-sorting_algorithms/0-bubble_sort.c
@@ -1,6 +1,19 @@
 #include "sort.h"
 
 /**
+ * swap_int - swaps two ints
+ * @a: 1st int to swap
+ * @b: 2nd int to swap
+ */
+void swap_int(int *a, int *b)
+{
+	int tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
+
+
+/**
  * bubble_sort - sort in ascending order
  * @array: array
  * @size: array length
