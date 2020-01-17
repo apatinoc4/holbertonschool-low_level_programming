f0eb9f8b80ba48809edd38862ebde8d395a656a4
root <root@DESKTOP-GQSD5ER.localdomain>
2020-01-16 21:34:48 -0500

    1 fix

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,30 +1,29 @@
 #include "search_algos.h"
 /**
  * binary_search - run the binary search function
- * @array: array
- * @size: size of array
- * @value: value 
- *
- * Return: value, or -1
+ * @array: sorted array to search
+ * @size: size of the array
+ * @value: value to search for
+ * Return: index of value, or -1
  */
 
 int binary_search(int *array, size_t size, int value)
 {
 	if (!array)
 		return (-1);
-	return (search(array, 0, size - 1, value));
+	return (b_search(array, 0, size - 1, value));
 }
 
 /**
- * search - search recursively using binary search
- * @array: array to search
- * @start: start
- * @end: end
- * @value: value to search 
- * Return: value or -1
+ * b_search - search recursively using binary search
+ * @array: sorted array to search
+ * @start: start of array
+ * @end: end of array
+ * @value: value to search for
+ * Return: index of value or
  */
 
-int search(int *array, size_t start, size_t end, int value)
+int b_search(int *array, size_t start, size_t end, int value)
 {
 	size_t mid, i = start;
 
@@ -49,9 +48,8 @@ int search(int *array, size_t start, size_t end, int value)
 	if (array[mid] == value)
 		return (mid);
 	if (array[mid] > value)
-		return (search(array, start, mid - 1, value));
+		return (b_search(array, start, mid - 1, value));
 	if (array[mid] < value)
-		return (search(array, mid + 1, end, value));
+		return (b_search(array, mid + 1, end, value));
 	return (-1);
-
 }
